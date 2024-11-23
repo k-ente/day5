d693fba717422e863560bfce18187e39aaae15bb
k-ente <126145871+k-ente@users.noreply.github.com>
2024-11-23 19:28:22 +0900

    test

---
+++ communication-example.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Header.Get("Content_Type") {
+		case "application/json":
+			var data map[string]interface{}
+			if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+		case "application/x-www-form-urlencoded":
+			if err := r.ParseForm(); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+
+			}
+
+		case "text/plain":
+			var body []byte
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+
+			}
+			fmt.Print(body)
+
+		}
+		fmt.Fprintf(w, "Hello World")
+	})
+	http.ListenAndServe(":8080", nil)
+}
+
+///test
