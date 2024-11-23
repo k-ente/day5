d7af66e10400358a0b3755dc6eb1f07043678da6
k-ente <126145871+k-ente@users.noreply.github.com>
2024-11-23 19:52:24 +0900

    おまけ

--- communication-example.go
+++ communication-example.go
@@ -7,35 +7,42 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Header.Get("Content_Type") {
-		case "application/json":
-			var data map[string]interface{}
-			if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-		case "application/x-www-form-urlencoded":
-			if err := r.ParseForm(); err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
-			}
-
-		case "text/plain":
-			var body []byte
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-
-			}
-			fmt.Print(body)
+	switch r.Header.Get("Content_Type") {
+	case "application/json":
+		var data map[string]interface{}
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	case "application/x-www-form-urlencoded":
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 
 		}
-		fmt.Fprintf(w, "Hello World")
-	})
+
+	case "text/plain":
+		var body []byte
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+
+		}
+		fmt.Print(body)
+
+	}
+	fmt.Fprintf(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/api", handler)
 	http.ListenAndServe(":8080", nil)
 }
 
